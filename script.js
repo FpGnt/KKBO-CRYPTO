e7abf6db108f51b8b952446ea43d28463cd540b9
FapAgent <valtsstrombergs@gmail.com>
2023-01-19 00:05:00 +0200

    new version

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
-// Check if the user is connected to MetaMask
-if (localStorage.getItem("connected") === "true") {
-    // Get the user's Ethereum address from LocalStorage
-    const address = localStorage.getItem("address");
-    // Change the button text to the user's Ethereum address
-    document.getElementById("connect-button").innerHTML = `Connected as ${address}`;
-}
-
-// Get the connect button
 const connectButton = document.getElementById("connect-button");
+const dropdownContent = document.getElementById("dropdown-content");
 
+if(localStorage.getItem("connected") === "true"){
+
+    address = localStorage.getItem("address")
+    
+    const dropdown = document.getElementById("dropdown");
+    const shortenedAddress = address.substring(0, 4) + "..." + address.substr(-5);
+    document.getElementById("connect-button").innerHTML = shortenedAddress;
+
+    connectButton.addEventListener("mouseover", () => {
+        if (localStorage.getItem("connected") === "true") {
+            dropdown.style.display = "block";
+            dropdownContent.classList.add("show");
+
+        }
+    });
+    
+    dropdown.addEventListener("mouseout", () => {
+        if (localStorage.getItem("connected") === "true") {
+            dropdown.style.display = "none";
+            dropdownContent.classList.remove("show");
+
+        }
+    });
+    const disconnectButton = document.getElementById("disconnect-button");
+
+    // Add an event listener to the disconnect button
+    disconnectButton.addEventListener("click", () => {
+        // Disable MetaMask
+        window.ethereum.disable();
+        // Clear the LocalStorage
+        localStorage.removeItem("connected");
+        localStorage.removeItem("address");
+        localStorage.removeItem("token");
+        location.reload();
+
+        document.getElementById("connect-button").innerHTML = "Connect Wallet";
+    });  
+
+}
 // Add an event listener to the button
 connectButton.addEventListener("click", () => {
     // Check if MetaMask is installed
-    if (typeof window.ethereum !== 'undefined') {
+    if (localStorage.getItem("connected") !== "true") {
 // Connect to MetaMask
 window.ethereum.enable().then(() => {
     const web3 = new Web3(window.ethereum);
@@ -23,12 +54,45 @@ window.ethereum.enable().then(() => {
             // Get the user's Ethereum address
             web3.eth.getAccounts().then(accounts => {
                 const address = accounts[0];
+
+                fetch("http://localhost:3000/login", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                        username: address,
+                    })
+                })
+                .then(res => res.json())
+                .then(data => {
+                    // Save token to local storage
+                    localStorage.setItem("token", data.token);
+                });
                 // Save the connection status and address in LocalStorage
                 localStorage.setItem("connected", "true");
                 localStorage.setItem("address", address);
+
+                const connectButton = document.getElementById("connect-button");
+                const dropdown = document.getElementById("dropdown");
+                connectButton.addEventListener("mouseover", () => {
+                    if (localStorage.getItem("connected") === "true") {
+                        dropdown.style.display = "block";
+                    }
+                });
+                
+                dropdown.addEventListener("mouseout", () => {
+                    if (localStorage.getItem("connected") === "true") {
+                        dropdown.style.display = "none";
+                    }
+                });
                 // Change the button text to the user's Ethereum address
-                document.getElementById("connect-button").innerHTML = `${address}`;
+                const shortenedAddress = address.substring(0, 4) + "..." + address.substr(-5);
+                document.getElementById("connect-button").innerHTML = shortenedAddress;;
+
+
             });
+            
+
+
         } else {
                     // The user is not connected to the Polygon network
                     // Create the modal element
@@ -61,35 +125,6 @@ window.ethereum.enable().then(() => {
                 }
             });
         });
-    } else {
-        // MetaMask is not installed
-        // Create the modal element
-        let modal = document.createElement("div");
-        modal.classList.add("modal");
-        modal.innerHTML = `
-            <div class="modal-content">
-                <span class="close">&times;</span>
-                <p>MetaMask is not installed. Please install MetaMask before continue</p>
-            </div>
-        `;
-        document.body.appendChild(modal);
-
-        // Get the modal element
-        let modalContent = document.getElementsByClassName("modal")[0];
-        // Get the <span> element that closes the modal
-        let span = document.getElementsByClassName("close")[0];
-        // When the user clicks on the button, open the modal
-        modalContent.style.display = "block";
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-            modalContent.style.display = "none";
-        }
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-            if (event.target == modalContent) {
-                modalContent.style.display = "none";
-            }
-        }
     }
 });
 
@@ -100,23 +135,23 @@ window.ethereum.on("accountsChanged", accounts => {
         // Clear the LocalStorage
         localStorage.removeItem("connected");
         localStorage.removeItem("address");
+        localStorage.removeItem("token");
         // Change the button text to "Connect"
         document.getElementById("connect-button").innerHTML = "Connect Wallet";
     }
 });
 
-
-// Get the connect button
-
 const disconnectButton = document.getElementById("disconnect-button");
 
-// Add an event listener to the disconnect button
-disconnectButton.addEventListener("click", () => {
-    // Disable MetaMask
-    window.ethereum.disable();
-    // Clear the LocalStorage
-    localStorage.removeItem("connected");
-    localStorage.removeItem("address");
-    // Change the button text to "Connect"
-    document.getElementById("connect-button").innerHTML = "Connect Wallet";
-});
+    // Add an event listener to the disconnect button
+    disconnectButton.addEventListener("click", () => {
+        // Disable MetaMask
+ 
+        // Clear the LocalStorage
+        localStorage.removeItem("connected");
+        localStorage.removeItem("address");
+        localStorage.removeItem("token");
+        location.reload();
+
+        document.getElementById("connect-button").innerHTML = "Connect Wallet";
+    });  
---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const secret = 'mysecretkey';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// Sample API endpoint that requires authentication
+app.get('/secret', authenticateToken, (req, res) => {
+    res.json({
+        message: 'This is a secret message'
+    });
+});
+
+// Authentication middleware
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, secret, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+}
+
+// Login endpoint
+app.post('/login', (req, res) => {
+    // Authenticate user and get token
+    const user = authenticate(req.body.username);
+    if (!user) return res.status(400).send('Invalid credentials');
+
+    // Create and assign token
+    const token = jwt.sign({ id: user.id }, secret);
+    res.json({ token });
+});
+
+// Dummy function to authenticate user
+function authenticate() {
+
+    return { id: 1 };
+}
+
+app.listen(3000, () => console.log('Server started'));
